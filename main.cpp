cab730b1a6caa95d3997a81c9b15cbb4ba14abe6
Raghav67816 <kumaraghav079@gmail.com>
2025-11-15 20:23:00 +0530

    improvement in frame calculation, still jitters

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_video.h>
+#include <SDL2/SDL_syswm.h>
 #include <vector>
 #include <string.h>
 #include <X11/Xlib.h>
+#include <X11/Xatom.h>
 
 using namespace std;
 
@@ -46,8 +48,26 @@ vector<SDL_Texture*> load_textures(SDL_Renderer* renderer){
     return textures;
 }
 
+void lower_window(SDL_Window *window){
+    SDL_SysWMinfo win_info;
+    if(SDL_GetWindowWMInfo(window, &win_info)){
+        Display *display = win_info.info.x11.display;
+        Window x_win = win_info.info.x11.window;
+
+        XLowerWindow(display, x_win);
+
+        Atom wm_type = XInternAtom(display, "_NET_WM_WINDOW_TYPE", False);
+        Atom wm_type_desktop = XInternAtom(display, "_NET_WM_WINDOW_TYPE_DESKTOP", False);
+
+        XFlush(display);
+    }
+}
+
 int main(int argc, char** args){
     int c_frame = 0;
+    const int frameDelay = 1000/30;
+    Uint32 frameStart;
+    int frameTime;
     WinDim dim = get_window_dim();
 
     if(SDL_Init(SDL_INIT_EVERYTHING | IMG_Init(IMG_INIT_JPG)) != 0){
@@ -56,15 +76,16 @@ int main(int argc, char** args){
 
 
     SDL_Window *window = SDL_CreateWindow(
-        "Wallpaper Engine", 0, 0, dim.width, dim.height, SDL_WINDOW_SHOWN
+        "Wallpaper Engine", 0, 0, dim.width, dim.height, 
+        SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS
     );
+
+    lower_window(window);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
         if(!renderer){
         cout << "failed to create renderer" << endl;
         cout << SDL_GetError() << endl;
     }
-
-
     SDL_Surface *surface = SDL_GetWindowSurface(window);
 
     vector<SDL_Texture*> frames = load_textures(renderer);
@@ -79,21 +100,26 @@ int main(int argc, char** args){
     bool running = true;
 
     while(running){
+        frameStart = SDL_GetTicks();
+
         while(SDL_PollEvent(&event)){
             if(event.type == SDL_QUIT){
                 running = false;
             }
         }
 
-        c_frame += 1;
-        if (c_frame >= 149){
-            c_frame = 0;
-        }
-        cout << c_frame << endl;
+        
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, frames[c_frame], NULL, NULL);
         SDL_RenderPresent(renderer);
-        SDL_Delay(1000);
+        
+        c_frame = (c_frame + 1) % frames.size();
+
+
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameDelay > frameTime){
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
 
     SDL_DestroyWindow(window);
