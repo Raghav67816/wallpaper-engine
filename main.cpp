9e87ce655c06c3fb27c7ccbda3f385bab0e2411d
Raghav67816 <kumaraghav079@gmail.com>
2025-11-15 19:17:49 +0530

    troubleshooting -> no textures loaded

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <SDL2/SDL_image.h>
+#include <vector>
+#include <string.h>
 
 using namespace std;
 
+vector<SDL_Texture*> load_textures(SDL_Renderer* renderer){
+    vector<SDL_Texture*> textures;
+    for (int i=0; i<150; i++){
+        string fname = "snowfall/" + to_string(i) + ".jpg";
+        cout << fname << endl;
+        SDL_Texture *texture = IMG_LoadTexture(
+            renderer, fname.c_str()
+        );
+        if(!texture){
+            cout << i << " not loaded" << endl;
+        }
+        textures.push_back(texture);
+    }
+    cout << "Done loading textures" << endl;
+    return textures;
+}
+
 int main(int argc, char** args){
-    if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+    if(SDL_Init(SDL_INIT_EVERYTHING | IMG_Init(IMG_INIT_JPG)) != 0){
         cout << "failed to init SDL" << endl;
     }
 
@@ -12,6 +32,9 @@ int main(int argc, char** args){
         "Wallpaper Engine", 0, 0, 800, 600, SDL_WINDOW_SHOWN
     );
     SDL_Surface *surface = SDL_GetWindowSurface(window);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    vector<SDL_Texture*> frames = load_textures(renderer);  
 
     SDL_UpdateWindowSurface(window);
 
